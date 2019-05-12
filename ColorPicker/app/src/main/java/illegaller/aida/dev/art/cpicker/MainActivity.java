fa135a2578c9870f42d598db6911728e6467d903
mwalanay@gmail.com <mwalanay@gmail.com>
2019-05-12 18:55:47 +0700

    initial commit

---
+++ ColorPicker/app/src/main/java/illegaller/aida/dev/art/cpicker/MainActivity.java
@@ -0,0 +1,14 @@
+package illegaller.aida.dev.art.cpicker;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
